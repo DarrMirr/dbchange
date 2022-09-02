2e9ee7e85d8bea60c94e6090029cc4bffe802473
Darr Mirr <polukeev.v.s@gmail.com>
2022-09-02 10:33:46 +0300

    DBC-1. prepare build gradle for maven central deployment.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,35 +1,55 @@
 plugins {
     `java-library`
     `maven-publish`
+    signing
 }
 
-group = "com.github.darrmirr"
+group = "io.github.darrmirr"
 version = project.property("project.version")!!
 
 repositories {
     mavenLocal()
     mavenCentral()
+    // for deployment to Maven Central
+    maven {
+        val releasesUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+        val snapshotsUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+        url = if (version.toString().endsWith("SNAPSHOT")) snapshotsUrl else releasesUrl
+        credentials {
+            username = project.properties["ossrhUsername"].toString()
+            password = project.properties["ossrhPassword"].toString()
+        }
+    }
 }
 
-dependencies {
-    api("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
-    api("org.slf4j:slf4j-api:1.7.36")
+// versions
+val h2Version = "2.1.214"
+val hamcrestVersion = "1.3"
+val junitVersion = "5.9.0"
+val log4jVersion = "2.18.0"
+val mockitoVersion = "4.7.0"
+val sl4jVersion = "1.7.36"
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+dependencies {
+    api("org.junit.jupiter:junit-jupiter-api:$junitVersion")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    api("org.slf4j:slf4j-api:$sl4jVersion")
+
+    // test scope
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitVersion")
     // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter-params
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
     // https://mvnrepository.com/artifact/org.hamcrest/hamcrest-all
-    testImplementation("org.hamcrest:hamcrest-all:1.3")
+    testImplementation("org.hamcrest:hamcrest-all:$hamcrestVersion")
     // https://mvnrepository.com/artifact/com.h2database/h2
-    testImplementation("com.h2database:h2:2.1.214")
+    testImplementation("com.h2database:h2:$h2Version")
     // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j
-    testImplementation("org.apache.logging.log4j:log4j:2.18.0")
+    testImplementation("org.apache.logging.log4j:log4j:$log4jVersion")
     // https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-slf4j-impl
-    testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:2.18.0")
+    testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
     // https://mvnrepository.com/artifact/org.mockito/mockito-core
-    testImplementation("org.mockito:mockito-core:4.7.0")
+    testImplementation("org.mockito:mockito-core:$mockitoVersion")
 }
 
 tasks.getByName<Test>("test") {
@@ -51,8 +71,42 @@ publishing {
             version = project.property("project.version").toString()
 
             from(components["java"])
+
+            pom {
+                packaging = "jar"
+                name.set(project.name)
+                url.set(project.property("project.url").toString())
+                description.set("Easy and declarative way to execute sql queries in JUnit tests.")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/DarrMirr/dbchange/blob/main/LICENSE")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:https://github.com/DarrMirr/dbchange.git")
+                    developerConnection.set("scm:git@github.com:DarrMirr/dbchange.git")
+                    url.set("https://github.com/DarrMirr/dbchange")
+                }
+
+                developers {
+                    developer {
+                        id.set("DarrMirr")
+                        name.set("Darr Mirr (Vladimir S. Polukeev)")
+                        email.set("polukeev.v.s@yandex.ru")
+                    }
+                }
+            }
         }
     }
+}
+
+// for deployment to Maven Central
+// Point to notice: it is mandatory that signing task follow to publishing one
+signing {
+    sign(publishing.publications[project.name])
 }
 
 /**
@@ -73,16 +127,23 @@ publishing {
  *                   <goal>install-file</goal>
  *               </goals>
  *               <configuration>
- *                   <file>${basedir}/libs/dbchange-1.0.0-SNAPSHOT.jar</file>
+ *                   <file>${basedir}/libs/dbchange-1.0.0.jar</file>
  *               </configuration>
  *          </execution>
  *       </executions>
  *    </plugin>
  */
 tasks.jar {
+    manifest {
+        attributes(mapOf("Implementation-Title" to project.name,
+                "Implementation-Version" to project.version,
+                "Implementation-URL" to project.property("project.url"),
+                "Implementation-Vendor" to project.property("project.developers")))
+    }
+    // for maven install plugin
     into("META-INF/maven/$project.group/$project.name") {
-        from (tasks.getByName("generatePomFileForDbchangePublication"))
-        rename ("pom-default.xml", "pom.xml")
+        from(tasks.getByName("generatePomFileForDbchangePublication"))
+        rename("pom-default.xml", "pom.xml")
     }
 }
 
